56d18c48cfd9c9f8accdc6c2ee08c67c6ab7c61b
zenith-noble01 <webdev686@gmail.com>
2023-03-28 03:15:30 +0100

    all routes for the user has been added

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -2,59 +2,95 @@ import User from "../models/user.js";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 
+// export async function createUser(req, res) {
+//   try {
+//     const { username, email, password } = req.body;
+
+//     //checkUser username in db
+//     const checkUsername = new Promise((resolve, reject) => {
+//       User.findOne({ username: username }, function (err, user) {
+//         if (err) reject(new Error(err.message));
+//         if (user) reject(new Error("User already exists"));
+
+//         resolve();
+//       });
+//     });
+
+//     //checkUser username in db
+//     const checkEmail = new Promise((resolve, reject) => {
+//       User.findOne({ email: email }, function (err, user) {
+//         if (err) reject(new Error(err.message));
+//         if (user) reject(new Error("User already exists"));
+
+//         resolve();
+//       });
+//     });
+
+//     Promise.all([checkUsername, checkEmail])
+//       .then(() => {
+//         if (!password) {
+//           return res.status(400).send("Password is required");
+//         }
+//         //
+//         bcrypt
+//           .hash(password, 10)
+//           .then((hass) => {
+//             const user = new User({
+//               username: username,
+//               email: email,
+//               password: hass,
+//             });
+//             user.save().then((result) => {
+//               res.status(200).send({
+//                 msg: "User created successful...",
+//                 // user: result,
+//               });
+//             });
+//           })
+//           .catch((err) => {
+//             res.status(500).send({ err });
+//           });
+//       })
+//       .catch((err) => {
+//         res.status(500).send(err.message);
+//       });
+//   } catch (error) {
+//     return res.status(500).send({ msg: error.message });
+//   }
+// }
+
 export async function createUser(req, res) {
   try {
     const { username, email, password } = req.body;
 
-    //checkUser username in db
-    const checkUsername = new Promise((resolve, reject) => {
-      User.findOne({ username: username }, function (err, user) {
-        if (err) reject(new Error(err.message));
-        if (user) reject(new Error("User already exists"));
+    // Check if user already exists in the database
+    const existingUser = await User.findOne({ $or: [{ username }, { email }] });
+    if (existingUser) {
+      return res.status(400).send("User already exists");
+    }
 
-        resolve();
-      });
+    // Check if password is provided
+    if (!password) {
+      return res.status(400).send("Password is required");
+    }
+
+    // Hash the password
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    // Create a new user object
+    const user = new User({
+      username,
+      email,
+      password: hashedPassword,
     });
 
-    //checkUser username in db
-    const checkEmail = new Promise((resolve, reject) => {
-      User.findOne({ email: email }, function (err, user) {
-        if (err) reject(new Error(err.message));
-        if (user) reject(new Error("User already exists"));
+    // Save the new user to the database
+    const savedUser = await user.save();
 
-        resolve();
-      });
-    });
-
-    Promise.all([checkUsername, checkEmail])
-      .then(() => {
-        if (!password) {
-          return res.status(400).send("Password is required");
-        }
-        //
-        bcrypt
-          .hash(password, 10)
-          .then((hass) => {
-            const user = new User({
-              username: username,
-              email: email,
-              password: hass,
-            });
-            user.save().then((result) => {
-              res.status(200).send({
-                msg: "User created successful...",
-                // user: result,
-              });
-            });
-          })
-          .catch((err) => {
-            res.status(500).send({ err });
-          });
-      })
-      .catch((err) => {
-        res.status(500).send(err.message);
-      });
+    // Return a success message
+    return res.status(200).send({ msg: "User created successfully" });
   } catch (error) {
+    // Return an error message
     return res.status(500).send({ msg: error.message });
   }
 }
@@ -63,34 +99,33 @@ export async function loginUser(req, res) {
   try {
     const { email, password } = req.body;
 
-    User.findOne({ email })
-      .then((user) => {
-        bcrypt
-          .compare(password, user.password)
-          .then((hash) => {
-            const token = jwt.sign(
-              {
-                userId: user._id,
-                email: user.email,
-              },
-              process.env.JWT_SECRET,
-              { expiresIn: "24h" }
-            );
+    // Find user by email
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).send({ msg: "User not found" });
+    }
 
-            res.status(200).send({
-              msg: "User successfully logged in",
-              token,
-              username: user.username,
-            });
-          })
-          .catch((err) => {
-            res.status(400).send({ msg: "Passwords don't match" });
-          });
-      })
-      .catch((err) => {
-        res.status(400).send({ msg: "User not found" });
-      });
+    // Compare passwords
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(400).send({ msg: "Passwords don't match" });
+    }
+
+    // Create JWT token
+    const token = jwt.sign(
+      { userId: user._id, email: user.email },
+      process.env.JWT_SECRET,
+      { expiresIn: "24h" }
+    );
+
+    // Return success message with token and username
+    return res.status(200).send({
+      msg: "User successfully logged in",
+      token,
+      username: user.username,
+    });
   } catch (error) {
+    // Return error message
     return res.status(500).send({ msg: error.message });
   }
 }
@@ -98,33 +133,42 @@ export async function loginUser(req, res) {
 export async function getUser(req, res) {
   try {
     const { username } = req.params;
-    User.findOne({ username }, function (err, user) {
-      if (err) return res.status(500).send({ err });
-      if (!user) return res.status(404).send({ msg: "User not found" });
 
-      const { password, ...others } = Object.assign({}, user.toJSON());
+    // Find user by username
+    const user = await User.findOne({ username });
+    if (!user) {
+      return res.status(404).send({ msg: "User not found" });
+    }
 
-      res.status(200).send(others);
-    });
+    // Remove password field from user object
+    const { password, ...others } = user.toObject();
+
+    // Return user object without password
+    return res.status(200).send(others);
   } catch (error) {
-    res.status(400).send({ msg: "User not found" });
+    // Return error message
+    return res.status(500).send({ msg: error.message });
   }
 }
 
 export async function getUserById(req, res) {
   try {
     const { userId } = req.params;
-    User.findById(userId)
-      .then((user) => {
-        const { password, ...others } = Object.assign({}, user.toJSON());
 
-        res.status(200).send(others);
-      })
-      .catch((error) => {
-        return res.status(404).send({ msg: "User not found" });
-      });
+    // Find user by ID
+    const user = await User.findById(userId);
+    if (!user) {
+      return res.status(404).send({ msg: "User not found" });
+    }
+
+    // Remove password field from user object
+    const { password, ...others } = user.toObject();
+
+    // Return user object without password
+    return res.status(200).send(others);
   } catch (error) {
-    res.status(400).send({ msg: "User not found" });
+    // Return error message
+    return res.status(500).send({ msg: error.message });
   }
 }
 
