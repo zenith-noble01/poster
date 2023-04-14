65969e1f4ea15824120f03f4e28722a1c9997813
zenith-noble01 <webdev686@gmail.com>
2023-04-14 16:11:53 +0100

    the notfound page was added

--- client/src/pages/NotFound.jsx
+++ client/src/pages/NotFound.jsx
@@ -1,63 +1,17 @@
 import React from "react";
+import "../Styles/notfound.scss";
+import { notFound } from "../Images";
+import { useLocation } from "react-router-dom";
 
 const NotFound = () => {
-  const [file, setFile] = React.useState(null);
-  const [resizedImage, setResizedImage] = React.useState(null);
+  const { pathname } = useLocation();
 
-  const resizeImage = ({ file, maxWidth, maxHeight }) => {
-    return new Promise((resolve, reject) => {
-      const img = new Image();
-      img.src = URL.createObjectURL(file);
-      img.onload = () => {
-        const canvas = document.createElement("canvas");
-        let width = img.width;
-        let height = img.height;
-
-        if (width > height) {
-          if (width > maxWidth) {
-            height *= maxWidth / width;
-            width = maxWidth;
-          }
-        } else {
-          if (height > maxHeight) {
-            width *= maxHeight / height;
-            height = maxHeight;
-          }
-        }
-
-        canvas.width = width;
-        canvas.height = height;
-
-        const ctx = canvas.getContext("2d");
-        ctx.drawImage(img, 0, 0, width, height);
-
-        canvas.toBlob((blob) => {
-          resolve(blob);
-        }, file.type);
-      };
-      img.onerror = (error) => {
-        reject(error);
-      };
-    });
-  };
-
-  const handleFileChange = async (event) => {
-    const maxWidth = 800;
-    const maxHeight = 600;
-    const file = event.target.files[0];
-    setFile(file);
-    const resizedImage = await resizeImage({ file, maxWidth, maxHeight });
-    setResizedImage(resizedImage);
-  };
-
-  console.log(resizedImage);
+  const path = pathname.split("/")[1];
 
   return (
-    <div>
-      <input type="file" onChange={handleFileChange} />
-      {resizedImage && (
-        <img src={URL.createObjectURL(resizedImage)} alt="Resized" />
-      )}
+    <div className="app__notfound">
+      <img src={notFound} alt="" />
+      <h1>Page NotFound @ {path} </h1>
     </div>
   );
 };
